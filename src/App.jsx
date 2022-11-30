72b1a8f5e7fcccf21d1c5c16634e4959c2e4293a
Sen Cao <caosen.cn@gmail.com>
2022-11-30 15:51:54 +0200

    home page name done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import { Navbar } from './components'
 import { About, Home, Projects } from './pages'
 
 function App() {
 
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/projects" element={<Projects />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
+    <>
+      <div className="pf__content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+      <Navbar />
+    </>
   )
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,41 @@
 import './home.css'
+import name from '../../assets/name-lg.png'
+import name_sm from '../../assets/name-sm.png'
+import { useEffect, useState } from 'react'
+
+function WindowSize() {
+  const [screen, setScreen] = useState(window.innerWidth)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreen(window.innerWidth)
+    }
+    window.addEventListener("resize", handleResize)
+    handleResize()
+    return window.removeEventListener("resize", handleResize)
+  },[screen])
+  return screen
+}
 
 export function Home() {
+
+  const theWidth = WindowSize()
+
+  function setPic() {
+    if (theWidth > 550) {
+      return <img src = {name} alt="Sen Cao" />
+    } else {
+      return <img src = {name_sm} alt="Sen Cao" />
+    }
+  }
+
   return (
-    <div>Home</div>
+    <div className="pf__home">
+      <div className="pf__home__name">
+        {setPic()}
+     </div>
+     <div className="pf__home__colors">
+     </div>
+    </div>
   )
 }
