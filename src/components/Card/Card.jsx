a2b87900abeadcbc5055dd2a3e113d7dd26dc4b7
Sen Cao <caosen.cn@gmail.com>
2022-12-01 19:49:23 +0200

    project page animation added

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,41 +1,44 @@
 import './card.css'
 import name from '../../assets/name-sm.png'
 
-export function Card() {
+export function Card({cover, title, line1, line2, line3, line4, line5, line6, line7, pic}) {
   return (
     <div className="pf__card">
+      <div className="pf__card__cover">
+        <img src={cover} alt="" />
+      </div>
       <div className="pf__card__header">
         <div className="pf__card__header__title">
           <a href="#" target="__blank">
-           <h1>Channel3</h1>
+           <h1>{title}</h1>
           </a>
         </div>
         <div className="pf__card__header__text">
           <p className="pf__card__header__text__line1" style={{opacity: '1'}}>
-            Line Content
+            {line1}
           </p>
           <p className="pf__card__header__text__line2" style={{opacity: '0.9'}}>
-            Line Content
+            {line2}
           </p>
           <p className="pf__card__header__text__line3" style={{opacity: '0.8'}}>
-            Line Content
+            {line3}
           </p>
           <p className="pf__card__header__text__line4" style={{opacity: '0.7'}}>
-            Line Content
+            {line4}
           </p>
           <p className="pf__card__header__text__line5" style={{opacity: '0.6'}}>
-            Line Content
+            {line5}
           </p>
           <p className="pf__card__header__text__line6" style={{opacity: '0.5'}}>
-            Line Content
+            {line6}
           </p>
           <p className="pf__card__header__text__line7" style={{opacity: '0.4'}}>
-            Line Content
+            {line7}
           </p>
         </div>
       </div>
       <div className="pf__card__pic">
-        <img src={name} alt="" />
+        <img src={pic} alt="" />
       </div>
     </div>
   )
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,30 +1,86 @@
 import { Card } from '../../components'
 import './projects.css'
-import { gsap } from 'gsap'
-import Flip from 'gsap/Flip'
-
-const cards = document.querySelectorAll('.pf__projects__card')
-cards.forEach((card, index) => {
-  card.addEventListener('click', ()=> {
-    const state = Flip.getState(cards)
-    const cardActive = card.classList.contains('active')
-
-    cards.forEach((otherCard, otherIndex) => {
-      otherCard.classList.remove('active')
-      otherCard.classList.remove('inactive')
-    })
-
-    if(!cardActive) card.classList.add('active')
-  })
-})
+import { useState, useRef, useEffect } from 'react'
+import c3 from '../../assets/logo-c3.png'
+import wf from '../../assets/logo-wf.png'
+import sm from '../../assets/logo-sg.png'
+import { TweenMax, Power3 } from 'gsap'
 
 export function Projects() {
   const left = "<"
   const right = ">"
   const left_finish = "</"
+
+  let textItem = useRef(null);
+  let noticeItem = useRef(null);
+  let cardItem = useRef(null);
+
+  useEffect(()=>{
+    TweenMax.to(
+      textItem,
+      .8,
+      {
+        opacity: 1,
+        y:110,
+        ease:Power3.easeOut
+      }
+    )
+    TweenMax.to(
+      noticeItem,
+      2,
+      {
+        opacity:1,
+        delay:1,
+        ease:Power3.easeOut
+      }
+    )
+    TweenMax.to(
+      cardItem,
+      4,
+      {
+        opacity:1,
+        y:20,
+        delay: .8,
+        ease:Power3.easeOut
+      }
+    )
+  },[])
+
+  const [active1, setActive1] = useState(false)
+  const [active2, setActive2] = useState(false)
+  const [active3, setActive3] = useState(false)
+
+
+  const clearClass = () => {
+    const cards = document.querySelectorAll('.pf__projects__card')
+    cards.forEach((card) => {
+      const needsClean = card.classList.contains('active')
+      if (needsClean) card.classList.remove('active')
+    })
+  }
+
+  const handelChange1 = () => {
+    clearClass()
+    setActive1(!active1)
+    setActive2(false)
+    setActive3(false)
+  }
+  const handelChange2 = () => {
+    clearClass()
+    setActive2(!active2)
+    setActive1(false)
+    setActive3(false)
+  }
+  const handelChange3 = () => {
+    clearClass()
+    setActive3(!active3)
+    setActive1(false)
+    setActive2(false)
+  }
+
   return (
     <div className='pf__projects section__padding'>
-      <div className="pf__projects__title">
+      <div className="pf__projects__title" ref={el => {textItem = el}}>
         <span className="sign">{left}</span>
         <span className="span">span</span>
         <span className="sign">{right}</span>
@@ -33,19 +89,20 @@ export function Projects() {
         <span className="span">span</span>
         <span className="sign">{right}</span>
       </div>
-      <div className="pf__projects__notice">
-        <p>*find more on my GitHub repo</p>
-        <p>**watch me presenting SharedGoal <a href="#">[here]</a></p>
+      <div className="pf__projects__notice" ref={el=>{
+      noticeItem = el}}>
+        <p>*find more on my <a href="#" target="__blank">[GitHub]</a> repo</p>
+        <p>**watch me presenting SharedGoal <a href="#" target="__blank">[here]</a></p>
       </div>
-      <div className="pf__projects__cards">
-        <div className="pf__projects__card active">
-          <Card />
+      <div className="pf__projects__cards" ref={el=> {cardItem = el}}>
+        <div className={`pf__projects__card ${active1 ? "active" : ""}`} onClick={handelChange1}>
+          <Card cover={sm} title="SharedGoal" line1="#Built with Ruby on Rails" line2="#Le Wagon final project " line3="Description" line4="#Web App that brings togetherness to solo learning process" line5="#Find your new goal for self improvement" line6="#Create your own learning group according to your preferences" line7="#Find your buddies to share self-improvement progress"  />
         </div>
-        <div className="pf__projects__card inactive">
-          <Card />
+        <div className={`pf__projects__card ${active2 ? "active" : ""}`} onClick={handelChange2}>
+          <Card cover={wf} title="WishFund" line1="#Self initialized project" line2="#Project lauching page built with React" line3="For detailed information please check on lauching page" line4="Description" line5="#Decentralization" line6="#Full Democracy" line7="#Secured Finance"/>
         </div>
-        <div className="pf__projects__card inactive">
-          <Card />
+        <div className={`pf__projects__card ${active3 ? "active" : ""}`} onClick={handelChange3}>
+          <Card cover={c3} title="Channel3" line1="#Self initialized project" line2="#Project lauching page built with React" line3="For detailed information please check on lauching page" line4="Description" line5="#Truly unbaised news by the AI" line6="#Everyone is a journist in their own life" line7="#Changing the traditional news and social media"/>
         </div>
       </div>
     </div>
