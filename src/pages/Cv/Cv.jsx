199367e763b14bb7739d1c186dd5a5f5a8049ce4
Sen Cao <caosen.cn@gmail.com>
2022-12-04 01:03:40 +0200

    pdf file size compressed

--- src/pages/Cv/Cv.jsx
+++ src/pages/Cv/Cv.jsx
@@ -3,13 +3,16 @@ import './Cv.css'
 
 export function Cv() {
   return (
-    <div>
-      <div className="pf__cv__left">
-        <a href="#"><p>Download CV</p></a>
+    <div className='pf__cv'>
+      <div className="container">
+        <div className="pf__cv__left">
+          <a href="#"><p>Download CV</p></a>
+        </div>
+
       </div>
       <Contact />
       <div className="pf__cv__pdf">
-        <iframe src="#" width="100%" height="500px"></iframe>
+        <iframe src="https://youtu.be/SAUIQHIqCVk" width="50%" height="500px"></iframe>
       </div>
     </div>
   )
