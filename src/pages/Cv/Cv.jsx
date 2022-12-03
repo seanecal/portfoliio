977c532b29ec22369aa92f6619c16264ef706dd3
Sen Cao <caosen.cn@gmail.com>
2022-12-04 00:52:05 +0200

    pdf file added

--- src/pages/Cv/Cv.jsx
+++ src/pages/Cv/Cv.jsx
@@ -8,6 +8,9 @@ export function Cv() {
         <a href="#"><p>Download CV</p></a>
       </div>
       <Contact />
+      <div className="pf__cv__pdf">
+        <iframe src="#" width="100%" height="500px"></iframe>
+      </div>
     </div>
   )
 }
